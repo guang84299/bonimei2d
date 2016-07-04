c20d8b105a5bd4a735c6525ddb6a52c4389e886a
严春光 <842997290@qq.com>
2016-07-04 15:44:38 +0800

    添加有米sdk

--- src/com/xugu/bonimei2d/AppActivity.java
+++ src/com/xugu/bonimei2d/AppActivity.java
@@ -123,7 +123,7 @@ public class AppActivity extends Cocos2dxActivity {
 	
 	public static void showAd(final int i)
 	{
-		QLAdController.getSpotManager().showSpotAd();
+		QLAdController.getSpotManager().showSpotAds(activity);
 	}
 	
 	
