93ccc1f7c5a6246350116c44c531fda1d018865f
严春光 <842997290@qq.com>
2016-07-13 16:06:23 +0800

    不含分享版本

--- src/com/xugu/bonimei2d/App.java
+++ src/com/xugu/bonimei2d/App.java
@@ -3,8 +3,6 @@ package com.xugu.bonimei2d;
 import android.app.Application;
 import android.os.Bundle;
 
-import com.umeng.fb.push.FeedbackPush;
-import com.umeng.socialize.PlatformConfig;
 
 /**
  * Created by ntop on 15/7/8.
@@ -13,17 +11,7 @@ public class App extends Application {
 
     @Override
     public void onCreate() {
-      FeedbackPush.getInstance(this).init(false);
         super.onCreate();
     }
 
-    //各个平台的配置，建议放在全局Application或者程序入口
-    {
-        //微信 wx12342956d1cab4f9,a5ae111de7d9ea137e88a5e02c07c94d
-        PlatformConfig.setWeixin("wx31d08c0693437c58", "efcdfccc6f036b5eed6f32473af3e2e9");
-        //新浪微博
-        //PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad");
-        PlatformConfig.setQQZone("1105493910", "1Hp8c0kEzTR1ZeLo");
- 
-    }
 }
--- src/com/xugu/bonimei2d/AppActivity.java
+++ src/com/xugu/bonimei2d/AppActivity.java
@@ -33,15 +33,7 @@ import com.excelliance.kxqp.sdk.IQueryUpdateCallback;
 import com.qinglu.ad.QLAdController;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.analytics.MobclickAgent.EScenarioType;
-import com.umeng.fb.ConversationActivity;
-import com.umeng.fb.FeedbackAgent;
-import com.umeng.socialize.ShareAction;
-import com.umeng.socialize.UMShareAPI;
-import com.umeng.socialize.UMShareListener;
-import com.umeng.socialize.bean.SHARE_MEDIA;
-import com.umeng.socialize.media.UMImage;
-import com.umeng.socialize.shareboard.SnsPlatform;
-import com.umeng.socialize.utils.ShareBoardlistener;
+
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -102,8 +94,7 @@ public class AppActivity extends Cocos2dxActivity {
 		activity.runOnUiThread(new Runnable() {		 
 		    @Override
 		    public void run() {
-		    	FeedbackAgent agent = new FeedbackAgent(activity);
-		    	agent.startFeedbackActivity();
+		    	Toast.makeText(activity, "test!!", 1).show();
 		    }
 		});
 	}
@@ -170,50 +161,18 @@ public class AppActivity extends Cocos2dxActivity {
 	
 	public static void share(int num)
 	{
-		UMImage image = new UMImage(activity, "http://pp.myapp.com/ma_icon/0/icon_42303229_1467172230/96");
-
-        String url = "http://android.app.qq.com/myapp/detail.htm?apkName=com.xugu.bonimei2d";
-        
-        new ShareAction(activity).setDisplayList(SHARE_MEDIA.WEIXIN,SHARE_MEDIA.WEIXIN_CIRCLE,SHARE_MEDIA.QQ,SHARE_MEDIA.QZONE)        
-        .withMedia(image)
-        .withTitle("拯救火柴人")
-        .withText("我已拯救"+num+"个火柴人，更多兄弟等你来救！")
-        .withTargetUrl(url)
-        .setCallback(umShareListener)
-        .open();    
-        
+		       
 	}
 	
 	public native static void shareSuccess();
 	public native static void sharefailure();
 	
-	private static UMShareListener umShareListener = new UMShareListener() {
-        @Override
-        public void onResult(SHARE_MEDIA platform) {       
-            Toast.makeText(activity, platform + " 分享成功啦!", Toast.LENGTH_SHORT).show();
-            shareSuccess();
-        }
-
-        @Override
-        public void onError(SHARE_MEDIA platform, Throwable t) {
-            Toast.makeText(activity,platform + " 分享失败啦!", Toast.LENGTH_SHORT).show();
-            sharefailure();
-        }
-
-        @Override
-        public void onCancel(SHARE_MEDIA platform) {
-            Toast.makeText(activity,platform + " 分享取消了!", Toast.LENGTH_SHORT).show();
-            sharefailure();
-        }
-
-        
-	};
+	
 	
 	
 	@Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         /** attention to this below ,must add this**/
-        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
     }
 }
